3758a18678f381de6c3c86677605737187e62aa8
Olexandr Ivaschenko <sasaivasenko9@gmail.com>
2025-04-23 22:11:55 +0300

    fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,5 +6,5 @@ const wallHight = wall.clientHeight;
 const wallWidth = wall.clientWidth;
 const coords = spider.getBoundingClientRect();
 
-spider.style.top += wallHight / 2 - coords.height / 2 + 'px';
-spider.style.left += wallWidth / 2 - coords.width / 2 + 'px';
+spider.style.top = wallHight / 2 - coords.height / 2 + 'px';
+spider.style.left = wallWidth / 2 - coords.width / 2 + 'px';
